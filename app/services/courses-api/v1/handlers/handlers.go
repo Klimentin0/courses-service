0d321aee7fd3b75e7f8b5e123bacf92889fcd757
Klimentin0 <meshkov.klim@gmail.com>
2025-01-31 20:21:38 +0300

    добавление ендпоинта на уровне приложения(app-layer). C соответствующим обёртыванием на бизенс уровнее (декодинг и размапливание страктов)

--- app/services/courses-api/v1/handlers/handlers.go
+++ app/services/courses-api/v1/handlers/handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"github.com/Klimentin0/courses-service/app/services/courses-api/v1/handlers/checkgrp"
 	"github.com/Klimentin0/courses-service/app/services/courses-api/v1/handlers/hackgrp"
+	"github.com/Klimentin0/courses-service/app/services/courses-api/v1/handlers/usergrp"
 	v1 "github.com/Klimentin0/courses-service/business/web/v1"
 	"github.com/Klimentin0/courses-service/foundation/web"
 )
@@ -18,5 +19,12 @@ func (Routes) Add(app *web.App, apiCfg v1.APIMuxConfig) {
 	checkgrp.Routes(app, checkgrp.Config{
 		Build: apiCfg.Build,
 		Log:   apiCfg.Log,
+	})
+
+	usergrp.Routes(app, usergrp.Config{
+		Build: apiCfg.Build,
+		Log:   apiCfg.Log,
+		DB:    apiCfg.DB,
+		Auth:  apiCfg.Auth,
 	})
 }
